94a85f2c2934b17ede5dfef7f54b231093ce4af9
Siddhanth <siddunishtu@gmail.com>
2023-08-20 22:29:58 +0530

    cleaned repo

--- A1/run.sh
+++
@@ -1,9 +0,0 @@
-bash compile.sh
-g++ check.cpp -o check.o
-bash interface.sh C "$1" out.txt
-bash interface.sh D out.txt final_out.txt
-./check.o "$1" final_out.txt
-# rm final_out.txt
-# rm out.txt
-# rm check.o
-# rm main.o
