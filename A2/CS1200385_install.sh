fe0f4bbd6f0fbe8ac621086e3cd1cdcb326b5e0b
SiddhanthRaja <79137035+SiddhanthRaja@users.noreply.github.com>
2023-09-23 19:15:37 +0530

    Update and rename 2020CS50443_install.sh to CS1200385_install.sh

--- A2/CS1200385_install.sh
+++ A2/CS1200385_install.sh
@@ -5,6 +5,7 @@ git clone https://github.com/barnawalabhinav/Data_Mining.git
 cd Data_Mining
 
 # Load HPC Modules
+module purge
 module load compiler/gcc/9.1.0
 module load compiler/python/3.6.0/ucs4/gnu/447
 module load pythonpackages/3.6.0/matplotlib/3.0.2/gnu
